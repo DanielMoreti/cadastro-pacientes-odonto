93a92ab4fbc641cb8d045b7becd1c96ef44c3bde
DanielMoreti <danielvitormoreti@hotmail.com>
2023-10-15 20:26:30 -0300

    criando navbar e footer com angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PacienteCadastroComponent } from './paciente-cadastro/paciente-cadastro.component';
 import { PacienteListaComponent } from './paciente-lista/paciente-lista.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';  
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [		
+  declarations: [					
     AppComponent,
       PacienteListaComponent,
-      PacienteCadastroComponent
+      PacienteCadastroComponent,
+      NavBarComponent,
+      FooterComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
