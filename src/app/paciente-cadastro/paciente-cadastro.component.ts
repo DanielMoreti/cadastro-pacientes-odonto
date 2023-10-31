663fa26332a838340269b0e434520d48a40eaa51
DanielMoreti <danielvitormoreti@hotmail.com>
2023-10-31 19:44:05 -0300

    cadastro e validação de formulário - Atividade 11

--- src/app/paciente-cadastro/paciente-cadastro.component.ts
+++ src/app/paciente-cadastro/paciente-cadastro.component.ts
@@ -3,6 +3,8 @@ import { FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '
 import { ErrorStateMatcher } from '@angular/material/core';
 import { PacienteCadastroEnderecoComponent } from '../paciente-cadastro-endereco/paciente-cadastro-endereco.component';
 import { ActivatedRoute } from '@angular/router';
+import { PacienteService } from '../services/paciente.service';
+import { Pacinete } from '../paciente-lista/paciente-lista.component';
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -21,10 +23,12 @@ export class PacienteCadastroComponent implements OnInit {
 
   @Output() cadastroPacienteFormGroup: FormGroup;
   @ViewChild(PacienteCadastroEnderecoComponent) childComponent: PacienteCadastroEnderecoComponent;
+  mockPacienteLocalStorage: Pacinete;
 
   matcher = new MyErrorStateMatcher();
+  paciente: any = {};
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private pacienteService: PacienteService) {
 
   }
 
@@ -36,24 +40,50 @@ export class PacienteCadastroComponent implements OnInit {
 
     this.route.params.subscribe(params => {
       const id = params['id'];
-      console.log(`ID: ${id}`);
     });
-
 
     this.cadastroPacienteFormGroup = new FormGroup({
       nomeFormControl: new FormControl('', [Validators.required]),
       idadeFormControl: new FormControl('', [Validators.required]),
       emailFormControl: new FormControl('', [Validators.required, Validators.email]),
-      telefoneFormControl: new FormControl('', [Validators.required]),
+      telefoneFormControl: new FormControl('', [Validators.required, Validators.pattern(/^\(\d{2}\)\s9?\d{4}-\d{4}$/)]),
       enderecoFormControl: new FormControl('', [Validators.required]),
     });
+
+    this.mostrarInformacaoCadastroTemporario();
   }
 
-  paciente: any = {};
+  mostrarInformacaoCadastroTemporario() {
+    this.mockPacienteLocalStorage = this.getPaciente();
+  }
 
-  cadastrarPaciente() {
-    console.log('Paciente cadastrado:', this.paciente);
+  public cadastrarPaciente(): void {
+    var paciente: Pacinete = {
+      id: 0,
+      nome: this.cadastroPacienteFormGroup.get('nomeFormControl')?.value,
+      idade: this.cadastroPacienteFormGroup.get('idadeFormControl')?.value,
+      email: this.cadastroPacienteFormGroup.get('emailFormControl')?.value,
+      telefone: this.cadastroPacienteFormGroup.get('telefoneFormControl')?.value,
+      endereco: this.cadastroPacienteFormGroup.get('enderecoFormControl')?.value,
+    }
 
-    this.paciente = {};
+    this.savePaciente(paciente);
+    this.mostrarInformacaoCadastroTemporario();
+  }
+
+  private savePaciente(pacinte: Pacinete): void {
+    const entity = pacinte;
+    this.pacienteService.saveEntity(entity, 'minhaEntidade');
+  }
+
+  // Método para obter uma entidade do localStorage
+  private getPaciente(): Pacinete {
+    const entity = this.pacienteService.getEntity('minhaEntidade');
+    return entity;
+  }
+
+  // Método para remover uma entidade do localStorage
+  private removePaciente(): void {
+    this.pacienteService.removeEntity('minhaEntidade');
   }
 }
